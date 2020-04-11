180fbbb8bbe3f53a21e76a5aea273518d22cd0f6
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-04-11 13:39:06 +0200

    🔧 Project tools init

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -25,6 +26,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           ></link>
           <link rel="icon" href="/logo-small.svg"></link>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          ></meta>
           {this.props.styleTags}
         </Head>
         <body>
