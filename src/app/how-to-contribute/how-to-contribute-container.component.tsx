7e38ecbaf747d13c104e9c6457b001359b99ef5b
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-04-28 14:20:16 +0200

    ✨ How to contribute header

---
+++ src/app/how-to-contribute/how-to-contribute-container.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HowToContributeHeader } from './header/how-to-contribute-header.component';
+
+export const HowToContributeContainer = () => {
+  return (
+    <>
+      <HowToContributeHeader />
+    </>
+  );
+};
--- src/pages/how-to-contribute.tsx
+++ src/pages/how-to-contribute.tsx
@@ -1,36 +1,8 @@
 import React from 'react';
-import { useDeviceDetect } from '../hooks/use-device-detect/use-device-detect.hook';
-import { LayoutBody } from '../layout/layout.styles';
+import { HowToContributeContainer } from '../app/how-to-contribute/how-to-contribute-container.component';
 
 const About = () => {
-  // TODO: Finish the page
-  const { isMobile, isDesktop } = useDeviceDetect();
-
-  return (
-    <>
-      <LayoutBody isMobile={isMobile} isDesktop={isDesktop}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        rutrum blandit ultricies. Morbi interdum commodo cursus. Maecenas a
-        tellus a arcu finibus tristique. Morbi venenatis faucibus massa. Mauris
-        ac faucibus lectus. Praesent cursus, ligula in aliquet volutpat, purus
-        metus pharetra quam, ut feugiat ex diam vehicula diam. Aliquam dui
-        turpis, tempor accumsan ultrices varius, pulvinar ut metus. Mauris
-        porttitor egestas lacinia. Curabitur elementum congue risus ut sodales.
-        Phasellus augue risus, finibus ut erat ac, tristique finibus nibh.
-        Phasellus vitae velit quis elit porta lacinia. Fusce at nisi nibh.
-        Mauris et ultricies diam, in condimentum neque. Nullam dictum, tellus
-        lobortis condimentum sagittis, metus eros dapibus nibh, at faucibus eros
-        eros vitae diam. Duis placerat lorem nulla, eu imperdiet velit suscipit
-        a. Morbi consequat dictum est, eu imperdiet nisl convallis sit amet.
-        Nullam imperdiet tempor libero et elementum. Duis a ex id felis euismod
-        faucibus. Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Morbi libero augue, sollicitudin ac fringilla
-        in, laoreet non quam. Proin finibus turpis auctor magna faucibus, sit
-        amet varius nunc euismod. Pellentesque ornare risus leo, sit amet
-        rhoncus felis venenatis at.
-      </LayoutBody>
-    </>
-  );
+  return <HowToContributeContainer />;
 };
 
 export default About;
